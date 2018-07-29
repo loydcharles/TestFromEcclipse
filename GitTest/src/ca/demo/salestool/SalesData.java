ac43e60a530cf24454def61076e2d01f48b6753f
Loyd Charles <loydcharles3@gmail.com>
2018-07-29 13:29:51 -0500

    Partial of implementation of displaying numbers

--- GitTest/src/ca/demo/salestool/SalesData.java
+++ GitTest/src/ca/demo/salestool/SalesData.java
@@ -1,9 +1,11 @@
 package ca.demo.salestool;
 
 public class SalesData {
-	int data[] = {0, 4, 42};
-	
+	int data[] = { 0, 4, 42 };
+
 	public void display() {
-		System.out.println("TODO: Implement data display");
+		for (int i = 0; i < data.length; i++) {
+			System.out.println(" " + i);
+		}
 	}
 }
